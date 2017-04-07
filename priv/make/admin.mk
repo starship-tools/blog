d7bd8203fe6afac12e252e90bc1e8bd2bca0daf5
Duncan McGreggor <oubiwann@gmail.com>
2017-04-06 23:42:03 -0500

    Added target for only publishing modifieds to AWS.

--- priv/make/admin.mk
+++ priv/make/admin.mk
@@ -2,6 +2,11 @@ publish-github:
 	@git commit docs -m "Regenerated site."
 	@git push origin master
 
-publish-aws:
-	aws --profile=starship s3 cp docs/ s3://starship.tools/ --recursive
+publish-aws-all:
+	@aws --profile=starship s3 cp docs/ s3://starship.tools/ --recursive
 
+publish-aws:
+	@for f in `git status|grep modified|awk '{print $$2}'|egrep '^docs/'` ; do \
+		aws --profile=starship s3 \
+			cp "$$f" s3://starship.tools/`echo $$f|sed -e 's/^docs\///'` ; \
+	done
